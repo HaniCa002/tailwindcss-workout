eff1fd1a051cf64f9f989f561afc1515618e8463
Honey <hediemalekii82@gmail.com>
2025-01-04 18:30:44 +0330

    start tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myc : {
+          blu1 : "#4DA1A9",
+          blu2 : "#79D7BE",
+          light1 : "#F6F4F0",
+          bgcol : "#2E5077"
+        }
+      }
+    },
   },
   plugins: [],
 }
